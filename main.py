0ae3c003ce3813cfed70865213c7307850517ad7
Gihan Gamage <gihangamage.15@cse.mrt.ac.lk>
2023-06-02 00:43:12 +1000

    Update main.py

--- main.py
+++ main.py
@@ -1,56 +1,32 @@
 import streamlit as st
-import langchain
-from langchain.text_splitter import RecursiveCharacterTextSplitter
+from langchain.vectorstores import Pinecone
 from langchain.embeddings.openai import OpenAIEmbeddings
-from langchain.vectorstores import Chroma
-from langchain import OpenAI, VectorDBQA
-from langchain.chains import RetrievalQAWithSourcesChain
-import PyPDF2
+from langchain.llms import OpenAI
+from langchain.chains.question_answering import load_qa_chain
 
-# st.image("./Data/Capture.PNG")
-
-#This function will go through pdf and extract and return list of page texts.
-def read_and_textify(files):
-    text_list = []
-    sources_list = []
-    for file in files:
-        pdfReader = PyPDF2.PdfReader(file)
-        #print("Page Number:", len(pdfReader.pages))
-        for i in range(len(pdfReader.pages)):
-          pageObj = pdfReader.pages[i]
-          text = pageObj.extract_text()
-          pageObj.clear()
-          text_list.append(text)
-          sources_list.append(file.name + "_page_"+str(i))
-    return [text_list,sources_list]
 
 # centered page layout
 st.set_page_config(layout="centered", page_title="Cooee - Document QA")
 st.header("Cooee - Public Extension")
 st.write("---")
 
-#file uploader
-# uploaded_files = st.file_uploader("Upload documents",accept_multiple_files=True, type=["txt","pdf"])
-# st.write("---")
-# if uploaded_files  is None:
-#     st.info(f"""Upload files to analyse""")
 
-# #Use vectorDB to QnA
-# elif uploaded_files:
-# st.write(str(len(uploaded_files)) + " document(s) loaded..")
-#get text from documents
-# textify_output = read_and_textify(uploaded_files)
-# documents = textify_output[0]
-# sources = textify_output[1]
-#manual chunking based on pages.
-# docs = documents
-
-persist_directory = './Vector_store/'
-
-#extract embeddings
 embeddings = OpenAIEmbeddings(openai_api_key = st.secrets["openai_api_key"])
-#vstore with metadata. Here we will store page numbers.
+index_name = "external-facing-cooee"
 
+index = Pinecone.from_existing_index(index_name, embeddings)
+
+def get_similiar_docs(query,k=2,score=False):
+  if score:
+    similar_docs = index.similarity_search_with_score(query,k=k)
+  else:
+    similar_docs = index.similarity_search(query,k=k)
+  return similar_docs
+
+# model_name = "text-davinci-003"
+model_name = "gpt-3.5-turbo"
+# model_name = "gpt-4"
+llm = OpenAI(model_name=model_name)
 vStore = Chroma(persist_directory=persist_directory, embedding_function=embeddings)
 st.write(vStore.get())
 #     vStore = Chroma.from_texts(docs, embeddings, metadatas=[{"source": s} for s in sources], persist_directory=persist_directory)
@@ -63,8 +39,21 @@ model_name = "gpt-3.5-turbo"
 
 # #initiate model
 llm = OpenAI(model_name=model_name, openai_api_key = st.secrets["openai_api_key"], streaming=True)
-# model = RetrievalQAWithSourcesChain.from_chain_type(llm=llm, chain_type="stuff", retriever=retriever)
-model = VectorDBQA.from_chain_type(llm=llm, chain_type="stuff", vectorstore=vStore)
+
+chain = load_qa_chain(llm, chain_type="stuff")
+
+def get_answer(query):
+  similar_docs = get_similiar_docs(query)
+  
+  answer =  chain.run(input_documents=similar_docs, question=query)
+  # print('Answer >>>>>>>>>>')
+  # print(answer)
+  # print("Relevant Documents >>>>>>>>>>")
+  sources = []
+  for d in similar_docs:
+    # print(d.metadata)
+    sources.append(d.metadata['source'])
+  return  {'Answer':answer,'Sources':sources}
 
 st.header("Ask your data")
 user_q = st.text_area("Enter your question here")
@@ -73,11 +62,11 @@ if st.button("Get Response"):
             # create gpt prompt
             # result = model.run(user_q)
             with st.spinner("Cooee is working on it..."):
-                result = model({"query": user_q}, return_only_outputs=True)
+                result = get_answer(user_q)
                 st.subheader('Your response:')
-                st.write(result)
+                st.write(result['Answer'])
                 st.subheader('Source pages:')
-                st.write(result['sources'])
+                st.write(result['Sources'])
         except Exception as e:
             st.error(f"An error occurred: {e}")
             st.error('Oops, the GPT response resulted in an error :( Please try again with a different question.')
